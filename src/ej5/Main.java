930ccb6adf5f025a0252d3a8ccc8ee449eccd0a6
enieves <enieves@106-02.info2>
2024-01-19 14:24:56 +0100

    test1

--- src/ej5/Main.java
+++ src/ej5/Main.java
@@ -3,16 +3,51 @@ package ej5;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//
+		int[] tabla = { 2, 3, 4, 5, 6, 7, 8, 2, 3 };
+		int[] tablaDevuelta;
+
+		//
+		int num;
+		
+		
 
 	}
 
 	int[] buscarTodos(int t[], int valor) {
-		int[] tabla;
-		
-		
-		
-		return tabla;
+		// numero que guarda la posicion de la tabla en donde
+		int posicion=0, aumento = 0;
+		//
+		int[] tablaAumento;
+
+		// recorre la tabla para crear el tamaño de la tabla
+		for (int i = 0; i < t.length; i++) {
+
+			// si encuentra un valor igual al de la tabla
+			if (valor == t[i]) {
+
+				aumento++;
+
+			}
+
+		}
+
+		tablaAumento = new int[aumento];
+
+		// recorre la tabla para introducir los valores en la tabla creada
+		for (int i = 0; i < t.length; i++) {
+
+			// si encuentra un valor igual al de la tabla
+			if (valor == t[i]) {
+
+				tablaAumento[posicion]=i;
+				posicion++;
+			}
+
+		}
+
+		return t;
 	}
-	
+
 }
---
+++ src/ej6/Main.java
@@ -0,0 +1,20 @@
+package ej6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//
+		int [] t= {10,1,5,8,9,2};
+		
+	}
+	
+	int[] suma(int t[], int numElementos) {
+		
+		//
+		
+		
+		return t;
+	}
+	
+}
